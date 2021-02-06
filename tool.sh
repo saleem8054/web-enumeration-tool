3ab3b7f5e6d6824818473923e81010b9d95404a9
saleem8054 <transparent79twister@gmail.com>
2021-02-06 20:37:03 +0100

    maximising subdomains count

--- tool.sh
+++ tool.sh
@@ -28,33 +28,15 @@ echo "[+] Harvesting subdomains with finddomain..."
 ./findomain-linux -t $url --quiet -r > $HOME/Desktop/$url/recon/finddomain.txt
 
 echo "[+] Harvesting subdomains with subfinder..."
-subfinder -d $url --silent -nW -all > $HOME/Desktop/$url/recon/subfinder.txt
+subfinder -d $url --silent -t 200 -nW -all > $HOME/Desktop/$url/recon/subfinder.txt
 
-echo "[+] Checking the maximum sub domains count"
+echo "[+] Maximising subdomains count..."
+cat $HOME/Desktop/$url/recon/subfinder.txt $HOME/Desktop/$url/recon/finddomain.txt | sort -u | egrep -iv "(www.$url)" > $HOME/Desktop/$url/recon/httprobe/alive.txt
 sleep 1
-findDomainCount=$(cat $HOME/Desktop/$url/recon/finddomain.txt | wc -l)
-subFinderCount=$(cat $HOME/Desktop/$url/recon/subfinder.txt |  wc -l)
-finalFile=""
-
-if [ $findDomainCount -gt $subFinderCount ]
-then
-	rm $HOME/Desktop/$url/recon/subfinder.txt
-	finalFile="finddomain.txt"
-elif [ $findDomainCount -lt $subFinderCount ]
-then
-	rm $HOME/Desktop/$url/recon/finddomain.txt
-	finalFile="subfinder.txt"
-else
-	rm $HOME/Desktop/$url/recon/finddomain.txt
-	finalFile="subfinder.txt"
-fi	
-
-cat $HOME/Desktop/$url/recon/$finalFile | egrep -iv "(www.$url)" > $HOME/Desktop/$url/recon/httprobe/alive.txt
-rm $HOME/Desktop/$url/recon/$finalFile
 
 echo "[+] Checking for possible subdomain takeover..."
-subfinder -d $url --silent -t 100 -all > $HOME/Desktop/$url/recon/final.txt
-subjack -w $HOME/Desktop/$url/recon/final.txt -t 100 -timeout 30 -ssl -c $HOME/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $HOME/Desktop/$url/recon/potential_takeovers/potential_takeovers.txt
+subfinder -d $url --silent -t 200 -all > $HOME/Desktop/$url/recon/final.txt
+subjack -w $HOME/Desktop/$url/recon/final.txt -t 200 -timeout 30 -m -a -ssl -v -o $HOME/Desktop/$url/recon/potential_takeovers/potential_takeovers.txt
 
 echo "[+] Checking CNAME records for subdomain takeover..."
 for domain in $(cat $HOME/Desktop/$url/recon/final.txt); do dig $domain | grep CNAME >> $HOME/Desktop/$url/recon/DNS/CNAME.txt; done
